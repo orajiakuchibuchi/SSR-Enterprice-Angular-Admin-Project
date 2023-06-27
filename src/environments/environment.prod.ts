5c0a5da8f63a31c005cac52584bd5a718200b13f
orajiakuchibuchi <somtobuchi@gmail.com>
2023-06-27 15:27:13 +0100

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   flutter_public_key: 'FLWPUBK_TEST-6f6045944f247c7d04c3b7c1d73a2723-X',
   flutter_private_key: 'FLWSECK_TEST-65474a7f10fa663422b0182f9cd9ed66-X',
-  api: 'http://node-data-server.echelinks.com',
+  api: 'https://node-data-server.echelinks.com',
   appName: 'Sparkling Green',
-  uploadApi: 'http://node-file-server.echelinks.com'
+  uploadApi: 'https://node-file-server.echelinks.com'
 };
